ccd795a57a92920ef784acca0918457658b21e65
otavioribeiromedeiros <91473105+otavioribeiromedeiros@users.noreply.github.com>
2022-10-11 09:36:37 -0300

    PLAT-217 add run ECS task script (#1)
    * PLAT-217 add run ECS task script
    
    * PLAT-217 build new distribution
    
    * chore: Update dist
    
    * PLAT-217 build new distribution
    
    * PLAT-217 build new distribution
    
    * PLAT-217 refactorings and improving logs
    
    * PLAT-217 refactorings and improving logs
    
    * PLAT-217 refactorings and improving logs
    
    * PLAT-217 refactoring
    
    * PLAT-217 README file
    
    * chore: Update dist
    
    * PLAT-217 tweak GA workflow
    
    * PLAT-217 fix task definition file path
    
    * Update dist folder
    
    * PLAT-217 fix task definition file path
    
    * PLAT-217 rename job
    
    * PLAT-217 tweak README
    
    * PLAT-217 tweak README
    
    Co-authored-by: GitHub Actions <runner@fv-az241-750.2tfxs3cvuluejhi1wdm4o5ef4c.bx.internal.cloudapp.net>
    Co-authored-by: GitHub Actions <runner@fv-az204-953.d0ggoinxmwcejgojbuqly40l0e.cx.internal.cloudapp.net>
    Co-authored-by: GitHub Actions <runner@fv-az201-613.yo4stti0dhyudgyjg01iu2obqd.jx.internal.cloudapp.net>

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# aws-ecs-run-task
-Runs an Amazon ECS task
+## Amazon ECS "Run task" Action for GitHub Actions
+
+Registers an Amazon ECS task definition and runs it on ECS.
+
+## Usage
+
+```yaml
+    - name: Run database schema migration
+      uses: Bravado-network/aws-ecs-run-task@v1
+      with:
+        task-definition: ${{ steps.task-def.outputs.task-definition }}
+        cluster: <cluster name>
+        subnet: <subnet>
+        security-group: <security group>
+        container-name: <container name>
+        command: <docker CMD>
+        wait-timeout-in-seconds: 300
+        wait-for-finish: true
+```
+
+### Task definition file
+
+You can download a previous task definition, modify it and then run the ECS task as follows:
+
+```sh
+- name: Download task definition
+  run: |
+    aws ecs describe-task-definition --task-definition my-task-definition \
+    --query taskDefinition > task-definition-file.json
+
+- name: Fill in the new image ID in task definition
+  id: task-def
+  uses: aws-actions/amazon-ecs-render-task-definition@v1
+  with:
+    task-definition: task-definition-file.json
+    container-name: <my-container-name>
+    image: ${{ steps.login-ecr.outputs.registry }}/my-app-ecr-repo:${{ github.sha }}
+
+- name: Run ECS task
+  uses: Bravado-network/aws-ecs-run-task@v1
+  with:
+    task-definition: ${{ steps.task-def.outputs.task-definition }}
+    cluster: <cluster name>
+    subnet: <subnet>
+    security-group: <security group>
+    container-name: <my-container-name>
+    command: <docker CMD>
+    wait-timeout-in-seconds: 300
+    wait-for-finish: true
+```
+## Credentials and Region
+
+Use [the `aws-actions/configure-aws-credentials` action](https://github.com/aws-actions/configure-aws-credentials) to configure the GitHub Actions environment with environment variables containing AWS credentials and your desired region.
---
+++ index.js
@@ -0,0 +1,88 @@
+import { ECSClient, RegisterTaskDefinitionCommand, RunTaskCommand, waitUntilTasksStopped } from "@aws-sdk/client-ecs";
+import fs from "fs"
+import path from "path"
+const core = require("@actions/core")
+
+const DEFAULT_WAIT_TIMEOUT_IN_SECONDS = 300
+
+const region = process.env.AWS_REGION
+const client = new ECSClient({ region });
+
+const registerNewTaskDefinition = async () => {
+  const taskDefinitionFile = core.getInput("task-definition", { required: true })
+  const taskDefinitionPath = path.isAbsolute(taskDefinitionFile) ?
+      taskDefinitionFile :
+      path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
+  const fileContent = fs.readFileSync(taskDefinitionPath, "utf8");
+  
+  core.info("Registering the task definition");
+
+  try {
+    const taskDefinitionCommandResult = await client.send(new RegisterTaskDefinitionCommand(JSON.parse(fileContent)))
+    const { family, revision } = taskDefinitionCommandResult.taskDefinition
+    core.info(`New Task definition URL: https://${region}.console.aws.amazon.com/ecs/v2/task-definitions/${family}/${revision}/containers`)
+    
+    return taskDefinitionCommandResult.taskDefinition.taskDefinitionArn
+  } catch (error) {
+    core.setFailed("Failed to register task definition in ECS: " + error.message);
+    core.info("Task definition contents:");
+    core.info(fileContent);
+    throw(error);
+  }
+}
+
+const runTask = async (taskDefinitionArn) => {
+  const cluster = core.getInput("cluster", { required: true })
+  const subnet = core.getInput("subnet", { required: true })
+  const securityGroup = core.getInput("security-group", { required: true })
+  const containerName = core.getInput("container-name", { required: true })
+  const command = core.getInput("command", { required: true }).split(" ")
+
+  const result = await client.send(new RunTaskCommand({ 
+    cluster: cluster,
+    taskDefinition: taskDefinitionArn,
+    count: 1,
+    launchType: "FARGATE",
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets: [subnet],
+        securityGroups: [securityGroup]
+      }
+    },
+    overrides: {
+      containerOverrides: [{ name: containerName, command }]
+    }
+  }))
+  
+  const taskId = result.tasks[0].taskArn.split(`${cluster}/`)[1]
+  core.info(`Task execution has started with command: ${command}. Watch the execution logs in AWS console: https://${region}.console.aws.amazon.com/ecs/v2/clusters/${cluster}/tasks/${taskId}/configuration/containers/${containerName}`);
+  return result
+}
+
+const run = async () => {
+  try {
+    const newTaskDefinitionArn = await registerNewTaskDefinition()
+    const runTaskResult = await runTask(newTaskDefinitionArn)
+
+    const waitForFinish = core.getInput("wait-for-finish") || false
+    if (waitForFinish) {
+      const cluster = core.getInput("cluster", { required: true })
+      const waitTimeoutInSeconds = parseInt(core.getInput("wait-timeout-in-seconds")) || DEFAULT_WAIT_TIMEOUT_IN_SECONDS
+
+      core.info(`Waiting for the task to complete. Will wait for ${waitTimeoutInSeconds / 60} minutes`);
+      const { state } = await waitUntilTasksStopped({
+        client: client,
+        maxWaitTime: waitTimeoutInSeconds,
+        minDelay: 5,
+        maxDelay: 5
+      }, { cluster: cluster, tasks: [runTaskResult.tasks[0].taskArn] })
+    
+      core.info(state)
+    }  
+  } catch (error) {
+    core.setFailed(error.message);
+    core.error(error.stack);
+  }
+}
+
+run()
