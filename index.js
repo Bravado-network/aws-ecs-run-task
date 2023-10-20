6de1e92a05a46eea0434a685f268afbfd973d572
Ivan Triputen <stealth_today@mail.ru>
2023-10-20 17:15:44 +0300

    catch-cancel-workflow

--- index.js
+++ index.js
@@ -104,6 +104,13 @@ const run = async () => {
       }, { cluster: cluster, tasks: [taskArn] })
     
       await checkECSTaskExistCode(cluster, taskArn)
+
+      // Listen for the SIGTERM signal
+      process.on('SIGTERM', () => {
+        console.log('Workflow was canceled. Performing cleanup or other actions here.');
+        // Implement your cleanup logic here, such as stopping processes, cleaning up resources, etc.
+        process.exit(0); // Exit the action gracefully
+      });
     }  
   } catch (error) {
     core.setFailed(error.message);
